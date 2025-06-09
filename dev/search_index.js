b8287b0e605b4c3107559883229ca30c4718003d
Documenter.jl <documenter@juliadocs.github.io>
2025-06-09 14:02:29 +0000

    build based on 59500e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CollatzConjecture","page":"Home","title":"CollatzConjecture","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CollatzConjecture.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
